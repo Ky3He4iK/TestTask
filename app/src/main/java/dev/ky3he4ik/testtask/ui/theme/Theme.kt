8a990e13200b945f8ca4e67bb602d91ff2bc8ab0
ky3he4ik <misha10311@yandex.ru>
2023-02-03 16:59:14 +0300

    Task is done

--- app/src/main/java/dev/ky3he4ik/testtask/ui/theme/Theme.kt
+++ app/src/main/java/dev/ky3he4ik/testtask/ui/theme/Theme.kt
@@ -1,28 +1,33 @@
 package dev.ky3he4ik.testtask.ui.theme
 
+import android.graphics.Color
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.core.graphics.toColorLong
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = DarkGray,
+//    onBackground = DarkGray,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
+    secondary = Teal200,
+    background = LightGray,
+//    onBackground = LightGray,
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
+    ,
     onSurface = Color.Black,
     */
 )
